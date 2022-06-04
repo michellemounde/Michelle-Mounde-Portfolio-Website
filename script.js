e4f3056ff8aec4a4453ce7a06f841ae63bfc71aa
Michelle Mounde <mitchmounde@gmail.com>
2022-06-04 17:16:07 +0300

    added HTML content

--- script.js
+++ script.js
@@ -2,3 +2,19 @@ const h2 = document.createElement("h2");
 h2.textContent = "This content added by Javascript";
 
 document.querySelector("body").appendChild(h2);
+
+h2.textContent = "Michelle Mounde.";
+
+
+const h3 = document.createElement("h3");
+h3.textContent = "I build websites.";
+
+const body = document.getElementsByTagName("body");
+
+document.body.appendChild(h3);
+
+
+const p = document.createElement("p");
+p.textContent = "I'm a software engineer specializing in back-end development. I can make your website with great functionality and design. Currently upskilling at Moringa School."
+
+document.body.appendChild(p);
