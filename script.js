b9d8d84e602766adb174d00398242e640f6955c6
Michelle Mounde <mitchmounde@gmail.com>
2022-06-04 19:07:41 +0300

    added navbar

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
+const body = document.getElementsByTagName("body");
+const welcome = document.getElementById("section");
+const projects = document.getElementById("projects");
+const contact = document.getElementById("contact");
+
+
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by Javascript";
-
-document.querySelector("body").appendChild(h2);
-
 h2.textContent = "Michelle Mounde.";
+
+document.body.querySelector("section#welcome").appendChild(h2);
 
 
 const h3 = document.createElement("h3");
 h3.textContent = "I build websites.";
 
-const body = document.getElementsByTagName("body");
-
-document.body.appendChild(h3);
+document.body.welcome.append(h3);
 
 
 const p = document.createElement("p");
-p.textContent = "I'm a software engineer specializing in back-end development. I can make your website with great functionality and design. Currently upskilling at Moringa School."
+p.id = "bio";
+p.textContent = "I'm a software engineer specializing in back-end development. I can make your website with great functionality and design. Currently upskilling at ";
 
-document.body.appendChild(p);
+document.body.welcome.append(p);
+
+
+const strong = document.createElement("strong");
+strong.id = "current-work";
+strong.textContent = "Moringa School.";
+
+document.body.welcome.append(strong);
+
+
+
